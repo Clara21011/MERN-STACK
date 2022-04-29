55366f4e67f5175dc17ade5ad1a415e2698384b2
Clara21011 <88057123+Clara21011@users.noreply.github.com>
2022-04-29 09:17:36 -0500

    Update About.jsx

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -28,22 +28,21 @@ const About = () => {
             <Box className={classes.banner}></Box>
             <Box className={classes.wrapper}>
                 <Typography variant="h3">Code for Interview</Typography>
-                <Typography variant="h5" className={classes.text}>I'm a Software Engineer based in India. 
-                    I've built websites, desktop applications and corporate software.<br />
+                <Typography variant="h5" className={classes.text}>Second try<br />
                     If you are interested, you can view some of my favorite projects here
                     <Box component="span" style={{ marginLeft: 5 }}>
-                        <Link href="https://github.com/kunaltyagi9" color="inherit" target="_blank"><GitHub /></Link>
+                        <Link href="https://github.com/Clara21011" color="inherit" target="_blank"><GitHub /></Link>
                     </Box>
                 </Typography>
                 <Typography variant="h5" className={classes.text}>
                     Need something built or simply want to have chat? Reach out to me on
                     <Box component="span" style={{ marginLeft: 5 }}>
-                        <Link href="https://www.instagram.com/codeforinterview/" color="inherit" target="_blank">
+                        <Link href="https://www.instagram.com" color="inherit" target="_blank">
                             <Instagram />
                         </Link>
                     </Box>  
                         or send me an Email 
-                        <Link href="mailto:codeforinterview@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
+                        <Link href="mailto:claragarcia5689@gmail.com?Subject=This is a subject" target="_blank" color="inherit">
                             <Email />
                         </Link>.
                 </Typography>
